8655b338192bce275d5932f89083dfba38703f29
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-05-31 20:07:55 -0600

    Added express validator and post controller for adding a category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/product");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const categories = await Category.find({}).sort({ name: 1 }).exec();
@@ -49,9 +50,37 @@ exports.category_create_get = asyncHandler(async (req, res, next) => {
 });
 
 //display category create form on POST
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED");
-});
+exports.category_create_post = [
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description must hace at lesat 10 characteres")
+    .trim()
+    .isLength({ min: 10 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create new category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  }),
+];
 
 //display category delete form on GET
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 //display list of all products
 exports.product_list = asyncHandler(async (req, res, next) => {
